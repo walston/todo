7a004bc81c7bc94e3d4c58913dd8d85129d4bfb1
treezrppl2 <gojiradestroy@gmail.com>
2016-05-22 14:11:18 -0700

    Admin can create & read users & items

--- modules/database.js
+++ modules/database.js
@@ -18,12 +18,34 @@ module.exports = {
   }
 };
 
-function userCreate () {
-
+function userCreate (user, callback) {
+  var username = user.handle;
+  var realname = user.name;
+  var query = 'INSERT INTO "users" ( "username", "realname" ) ' +
+  'VALUES ( \'' + username + '\', \'' + realname + '\' );'
+  pg.connect(url, function(err, client, done) {
+    if (!err) {
+      client.query(query, function(err, result) {
+        done();
+        if (!err) callback(result);
+        else callback(new Error(err));
+      });
+    }
+  });
 }
 
-function userRead () {
-
+function userRead (username, callback) {
+  var query = 'SELECT * FROM "users" ' +
+  'WHERE "username" = \'' + username + '\'';
+  pg.connect(url, function(err, client, done) {
+    if (!err) {
+      client.query(query, function(err, result) {
+        done();
+        if (!err) callback(result);
+        else callback(new Error(err));
+      });
+    }
+  });
 }
 
 function userUpdate () {
@@ -34,12 +56,36 @@ function userDelete () {
 
 }
 
-function itemCreate () {
-
+function itemCreate (item, callback) {
+  var username = item.username;
+  var text = item.text;
+  var date = item.date || null;
+  var done = item.done || false;
+  var query = 'INSERT INTO "users" ( "username", "text", "date", "done" ) ' +
+  'VALUES ( \'' + username + '\', \'' + text + '\', \'' + date + '\', \'' + done + '\' );'
+  pg.connect(url, function(err, client, done) {
+    if (!err) {
+      client.query(query, function(err, result) {
+        done();
+        if (!err) callback(result);
+        else callback(new Error(err));
+      });
+    }
+  });
 }
 
 function itemRead () {
-
+  var query = 'SELECT * FROM "items" ' +
+  'WHERE "username" = \'' + username + '\'';
+  pg.connect(url, function(err, client, done) {
+    if (!err) {
+      client.query(query, function(err, result) {
+        done();
+        if (!err) callback(result);
+        else callback(new Error(err));
+      });
+    }
+  });
 }
 
 function itemUpdate () {
