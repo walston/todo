d2380014ec47a350766d4f223ce4baf208319d94
treezrppl2 <gojiradestroy@gmail.com>
2016-05-22 14:09:06 -0700

    Add username to items table

--- models/initialize.js
+++ models/initialize.js
@@ -6,8 +6,8 @@ var url = process.env.DATABASE_URL;
 module.exports.create = function(table) {
   pg.connect(url, function(err, client, done) {
     client.query(
-      'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, date timestamp without time zone, done BOOLEAN);' +
-      'CREATE TABLE users(id SERIAL PRIMARY KEY, username VARCHAR(16) not null, realname VARCHAR(60) not null);'
+      'CREATE TABLE items(id SERIAL PRIMARY KEY, username VARCHAR(16) NOT NULL, text VARCHAR(40) NOT NULL, date timestamp without time zone, done BOOLEAN);' +
+      'CREATE TABLE users(id SERIAL PRIMARY KEY, username VARCHAR(16) NOT NULL, realname VARCHAR(60) NOT NULL);'
     , function() {
       done();
     })
