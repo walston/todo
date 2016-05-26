3ac9a056a3fd6a6c3cd7c0831667322af1fc7939
treezrppl2 <gojiradestroy@gmail.com>
2016-05-26 09:52:50 -0700

    Schema updated

--- models/initialize.js
+++ models/initialize.js
@@ -4,11 +4,21 @@ var pg = require('pg');
 var url = process.env.DATABASE_URL;
 
 module.exports.create = function(table) {
+  var query =
+  'CREATE TABLE users(' +
+    'id SERIAL PRIMARY KEY, ' +
+    'username VARCHAR(16) NOT NULL, ' +
+    'realname VARCHAR(60) NOT NULL ' +
+  ');' +
+  'CREATE TABLE items(' +
+    'id SERIAL PRIMARY KEY, ' +
+    'userid INTEGER REFERENCES users (id), ' +
+    'text VARCHAR(40) NOT NULL, ' +
+    'date timestamp without time zone, ' +
+    'done BOOLEAN ' +
+  ');'
   pg.connect(url, function(err, client, done) {
-    client.query(
-      'CREATE TABLE items(id SERIAL PRIMARY KEY, username VARCHAR(16) NOT NULL, text VARCHAR(40) NOT NULL, date timestamp without time zone, done BOOLEAN);' +
-      'CREATE TABLE users(id SERIAL PRIMARY KEY, username VARCHAR(16) NOT NULL, realname VARCHAR(60) NOT NULL);'
-    , function() {
+    client.query(query, function() {
       done();
     })
   });
