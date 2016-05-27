95a23f8704d1bf86ab19966edfa5ccdabce886c0
treezrppl2 <gojiradestroy@gmail.com>
2016-05-27 15:19:18 -0700

    Setup for Travis

--- models/initialize.js
+++ models/initialize.js
@@ -2,7 +2,9 @@ var dotenv = require('dotenv').config();
 var pg = require('pg');
 var url = process.env.DATABASE_URL;
 
-module.exports.create = function(table) {
+create();
+
+function create() {
   var query =
   'CREATE TABLE users(id SERIAL PRIMARY KEY, username VARCHAR(16) NOT NULL, realname VARCHAR(60) NOT NULL );' +
   'CREATE TABLE items(id SERIAL PRIMARY KEY, userid INTEGER REFERENCES users (id), text VARCHAR(40) NOT NULL, date timestamp without time zone, done BOOLEAN );' +
@@ -12,19 +14,18 @@ module.exports.create = function(table) {
   "INSERT INTO items(userid, text, date, done) VALUES (3, 'Insert an item on initialization', null, false);"
   ;
   pg.connect(url, function(err, client, done) {
-    if (err) console.log(err);
-    client.query(query, function(err, results) {
-      if (err) console.log(err);
-      else console.log(results);
+    if (err) {
+      console.log(err);
       done();
-    })
-  });
-}
-
-module.exports.delete = function() {
-  pg.connect(url, function(err, client, done) {
-    client.query("DROP TABLE items; DROP TABLE users;", function() {
-      done();
-    });
+      pg.end();
+    }
+    else {
+      client.query(query, function(err, results) {
+        if (err) console.log(err);
+        else console.log(results);
+        done();
+        pg.end();
+      });
+    }
   });
 }
